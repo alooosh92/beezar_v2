28dc0d19d7268f1f4e9d17ad2199b8c003a02672
alaa baaj <the.lost.admiral@gmail.com>
2023-10-18 13:23:32 +0300

    commit

--- lib/screen/add_item/add_image_to_item.dart
+++ lib/screen/add_item/add_image_to_item.dart
@@ -4,8 +4,7 @@ import 'package:beezar_v2/screen/add_item/widget/appbar_add_item.dart';
 import 'package:beezar_v2/screen/add_item/widget/image_viewer.dart';
 import 'package:beezar_v2/screen/add_item/widget/row_add_and_delete_image.dart';
 import 'package:beezar_v2/screen/contact_us/widget/button_send.dart';
-import 'package:beezar_v2/screen/home/home_controller.dart';
-import 'package:beezar_v2/screen/home/primer_screen.dart';
+import 'package:beezar_v2/screen/show_all_item/show_all_item.dart';
 import 'package:beezar_v2/widget/progress_def.dart';
 import 'package:beezar_v2/widget/progress_home_row.dart';
 import 'package:beezar_v2/widget/row_number_screen_register.dart';
@@ -51,9 +50,7 @@ class AddImageToItem extends StatelessWidget {
                             Get.snackbar("معلومات", "تم اضافة الاعلان بنجاح ",
                                 backgroundColor: ColorManager.primaryColor,
                                 colorText: ColorManager.white);
-                            HomeController homeController = Get.find();
-                            await homeController.getAllItems(true);
-                            Get.offAll(const PrimerScreen());
+                            Get.offAll(const ShowAllItem());
                           } else {
                             Get.snackbar(
                                 "خطأ", "هناك خطأ ما الرجاء الاتصال بالمسؤول",
@@ -66,9 +63,7 @@ class AddImageToItem extends StatelessWidget {
                             Get.snackbar("معلومات", "تم تعديل الاعلان بنجاح ",
                                 backgroundColor: ColorManager.primaryColor,
                                 colorText: ColorManager.white);
-                            HomeController homeController = Get.find();
-                            await homeController.getAllItems(true);
-                            Get.offAll(const PrimerScreen());
+                            Get.offAll(const ShowAllItem());
                           } else {
                             Get.snackbar(
                                 "خطأ", "هناك خطأ ما الرجاء الاتصال بالمسؤول",
