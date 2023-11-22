d420c010795c61cbd19afbf0612a1f7de3ae55f5
SheymaFathy <freelance.ss22@gmail.com>
2023-11-22 11:23:35 +0200

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-//2023-10-28 alaa
+//2023-11-22 edit comments & rating app
 import 'dart:io';
 import 'package:beezer_v2/res/binding_def.dart';
 import 'package:beezer_v2/screen/auth/splash/splash_screen.dart';
